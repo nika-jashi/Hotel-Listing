e27c1481e0db214ab3a97a1839d0219e9f95bbe0
nika jashi <jashi.nika1@gmail.com>
2023-04-13 15:35:07 +0400

    simple CRUD for country model

--- HotelListingAPI/Data/HotelListingDbContext.cs
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using HotelListingAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HotelListingAPI.Data
 {
@@ -12,5 +13,59 @@ namespace HotelListingAPI.Data
         public DbSet<Hotel> Hotels { get; set; }
 
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+            new Country
+            {
+                Id = 1,
+                Name = "Jamaica",
+                ShortName = "JM",
+            },
+            new Country
+
+            {
+                Id = 2,
+                Name = "Bahamas",
+                ShortName = "BS",
+            },
+            new Country
+
+            {
+                Id = 3,
+                Name = "Cayman Island",
+                ShortName = "CI",
+            }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                 new Hotel
+                 {
+                     Id = 1,
+                     Name = "Sandals Resort and Spa",
+                     Address = "Negril",
+                     CountryId = 1,
+                     Rating = 4.5
+                 },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+                );
+        }
     }
 }
